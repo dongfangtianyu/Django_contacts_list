0022a1ae938b7f763c09cff7259b9522fd4e28da
Tianyu <Tianyu@qq.com>
2015-07-31 14:23:20 +0800

    删掉垃圾代码

--- contacts_list/admin.py
+++ contacts_list/admin.py
@@ -2,7 +2,4 @@ from django.contrib import admin
 from contacts_list.models import Contact
 
 
-class ContactAdmin(admin.ModelAdmin):
-    ss ='1'
-
-admin.site.register(Contact, ContactAdmin)
+admin.site.register(Contact)
--- contacts_list/froms.py
+++
@@ -1,7 +0,0 @@
-from contacts_list.models import *
-from django.forms import forms, ModelForm
-class ContactFrom(forms.Form):
-    class Meta:
-        model = Contact
-        fields = ('name','sex')
-SS = forms.M
